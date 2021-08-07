7de75d573c3605b9979f94d32285b122991a783a
Leo <leojvainio@gmail.com>
2021-08-07 20:17:08 +0200

    hej

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ var textToBeTyped = "Leffecheats [Version 10.0.10240]\n" +
 "Leg 40x? Make that something of the past, with leffecheats you're " +
 "guaranteed to get all those pistol achievements you've been dreaming " +
 "about!\n\n" +
-"C:\\Users\\Hackerboi>" +
+"C:\\Users\\Hackerboi>start leffecheats.exe\n" +
 "You have two options: Install leffecheats today or be reported to agency for immediate deportation ;)"
 var index = 0, isAdding = true
 
