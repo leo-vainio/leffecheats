c4eb7e23b927dbe090c7965a5b8088ef203d5f29
Leo <leojvainio@gmail.com>
2021-08-08 11:31:10 +0200

    added sound

--- src/main.js
+++ src/main.js
@@ -7,9 +7,9 @@ var textToBeTyped = "Leffecheats [Version 10.0.10240]\n" +
 "C:\\Program Files (x86)\\Origin Games\\Battlefield 4> start leffecheats.exe\n" +
 "Hi there! Welcome to leffecheats.com! Battlefield 4's " +
 "best kept secret. Tired of not getting a 100 kill streak with a Mare's " +
-"Leg 40x? Make that something of the past, with leffecheats you're " +
+"Leg 40x? Make that something of the past, with Leffecheats you're " +
 "guaranteed to get all those pistol achievements you've been dreaming about!\n\n" +
-"You have two options: Install leffecheats today or be reported to agency for immediate deportation ;)\n\n" +
+"You have two options: Install Leffecheats today or be reported to agency for immediate deportation ;)\n\n" +
 "C:\\Program Files (x86)\\Origin Games\\Battlefield 4>"
 
 var index = 0, isAdding = true, delay = 0
